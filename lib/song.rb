35405f25a5085830d48fe6262874ea1bdb40fbc7
Savannah C <sgcorrell@gmail.com>
2019-04-02 18:04:31 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize
+    @@count += 1
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    
+  end
+  
+end
