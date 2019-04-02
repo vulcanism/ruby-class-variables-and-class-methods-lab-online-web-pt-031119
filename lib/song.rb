45cb329829f59280d13c4a2c5ddfb5be08b50b9d
Savannah C <sgcorrell@gmail.com>
2019-04-02 19:09:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,9 +27,9 @@ class Song
   end
   
   def self.genre_count
-    genre_count = {}
-    @@genres.each {|genre| genre_count[genre] += 1}
-    genre_count
+    genre_hash = Hash.new(0)
+    @@genres.each {|genre| genre_hash[genre] += 1}
+    genre_hash
   end
   
 end
